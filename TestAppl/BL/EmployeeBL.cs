abcd5ebe9a2c1574dc9851a57ace2d922ef177f2
Keyur <keyur.k@advanced-india.com>
2016-01-05 12:15:54 +0530

    Added business logic.

---
+++ TestAppl/BL/EmployeeBL.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+
+namespace TestAppl.BL
+{
+    public class EmployeeBL
+    {
+        /// <summary>
+        /// Returns the employee name based on employee id
+        /// </summary>
+        /// <param name="employeeId">The employee id</param>
+        /// <returns>The employee name</returns>
+        public string GetEmployee(int employeeId)
+        {
+            string retVal = String.Empty;
+
+            switch (employeeId)
+            {
+                case 1:
+                    retVal = "Smith";
+                    break;
+
+                case 2:
+                    retVal = "Jacques";
+                    break;
+
+                case 3:
+                    retVal = "Dale";
+                    break;
+            }
+
+            return retVal;
+        }
+
+        /// <summary>
+        /// Returns the date of birth for the prvided employee id
+        /// </summary>
+        /// <param name="employeeId">The employee id</param>
+        /// <returns>The date of birth</returns>
+        public DateTime? GetDOB(int employeeId)
+        {
+            DateTime? retVal = null;
+
+            switch (employeeId)
+            {
+                case 1:
+                    retVal = new DateTime(1980, 01, 01);
+                    break;
+
+                case 2:
+                    retVal = new DateTime(1980, 02, 02);
+                    break;
+
+                case 3:
+                    retVal = new DateTime(1980, 02, 02);
+                    break;
+            }
+
+            return retVal;
+        }
+    }
+}
--- TestAppl/Program.cs
+++ TestAppl/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using TestAppl.BL;
 
 namespace TestAppl
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            var employeeBL = new EmployeeBL();
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Employee Information");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Name : " + employeeBL.GetEmployee(1));
+            Console.WriteLine("DOB  : " + Convert.ToDateTime(employeeBL.GetDOB(1)).ToShortDateString());
+
             Console.ReadLine();
         }
     }
